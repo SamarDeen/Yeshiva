97845e8667197ec2b4036302b2d9c293bb9a9624
Samar Deen <sa462@cornell.edu>
2024-04-07 22:34:07 -0400

    Create CatClassifier.py

---
+++ CatClassifier.py
@@ -0,0 +1,22 @@
+from fastcore.all import *
+from fastai.vision.all import *
+import streamlit as st
+
+## LOAD MODEl
+learn_inf = load_learner('/content/drive/My Drive/Yeshiva/FastAI_Lecture1/catbird_learner.pkl')
+## CLASSIFIER
+def classify_img(data):
+    pred, pred_idx, probs = learn_inf.predict(data)
+    return pred, probs[pred_idx]
+## STREAMLIT
+st.title("Cat & Bird Classifier! 🍓😻")
+bytes_data = None
+uploaded_image = st.file_uploader("Upload the image you want to classify")
+if uploaded_image:
+    bytes_data = uploaded_image.getvalue()
+    st.image(bytes_data, caption="Uploaded image")   
+if bytes_data:
+    classify = st.button("CLASSIFY!")
+    if classify:
+        label, confidence = classify_img(bytes_data)
+        st.write(f"It is a {label}! ({confidence:.04f})")
